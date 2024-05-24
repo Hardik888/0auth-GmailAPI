b7dd6558b60dda042dfe01329e6445f7e1b1cf69
hardik888 <hardik89990@gmail.com>
2024-05-24 04:01:01 -0400

    google oauth init

---
+++ googleauthservice.ts
@@ -0,0 +1,78 @@
+import { promises as fs } from "fs";
+import path from "path";
+import process from "process";
+import { OAuth2Client } from "google-auth-library";
+import { authenticate } from "@google-cloud/local-auth";
+import { google } from "googleapis";
+
+// If modifying these scopes, delete token.json.
+const SCOPES = [
+  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/gmail.send",
+];
+
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first time.
+const TOKEN_PATH = path.join(process.cwd(), "./credentials/token.json");
+const CREDENTIALS_PATH = path.join(
+  process.cwd(),
+  "./credentials/credentials.json"
+);
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client | null>}
+ */
+async function loadSavedCredentialsIfExist(): Promise<OAuth2Client | null> {
+  try {
+    const content = await fs.readFile(TOKEN_PATH, { encoding: "utf8" });
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials) as OAuth2Client;
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client: OAuth2Client): Promise<void> {
+  const content = await fs.readFile(CREDENTIALS_PATH, { encoding: "utf8" });
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: "authorized_user",
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload, { encoding: "utf8" });
+}
+
+/**
+ * Load or request authorization to call APIs.
+ *
+ * @return {Promise<OAuth2Client>}
+ */
+async function authorize(): Promise<OAuth2Client> {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = (await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  })) as OAuth2Client;
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+authorize().then(() => {
+  console.log("token generated");
+});
